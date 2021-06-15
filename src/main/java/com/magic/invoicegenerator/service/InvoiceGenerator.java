447286844f4f7bda8e57b4e98fb638874258e817
Rajnish Sahu <rajnishsahu95@gmail.com>
2021-06-15 14:58:26 +0530

    Add-display welcome message

---
+++ src/main/java/com/magic/invoicegenerator/service/InvoiceGenerator.java
@@ -0,0 +1,5 @@
+package com.magic.invoicegenerator.service;
+
+public class InvoiceGenerator {
+
+}
---
+++ src/test/java/com/magic/invoicegenerator/InvoiceGeneratorTest.java
@@ -0,0 +1,13 @@
+package com.magic.invoicegenerator;
+
+import com.magic.invoicegenerator.service.InvoiceGenerator;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class InvoiceGeneratorTest {
+
+    @Test
+    public void dummy() {
+        System.out.println("Welcome to Invoice Generator");
+    }
+}
