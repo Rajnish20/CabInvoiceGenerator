816d174f32852c89c6cec90f65cd7a659884916c
Rajnish Sahu <rajnishsahu95@gmail.com>
2021-06-15 15:02:16 +0530

    Add-calculateFare takes distance and time and returns totalfare

--- src/test/java/com/magic/invoicegenerator/InvoiceGeneratorTest.java
+++ src/test/java/com/magic/invoicegenerator/InvoiceGeneratorTest.java
@@ -10,4 +10,22 @@ public class InvoiceGeneratorTest {
     public void dummy() {
         System.out.println("Welcome to Invoice Generator");
     }
+
+    @Test
+    public void givenTimeAndDistance_ShouldReturnTotalFare() {
+        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+        double distance = 5;
+        int time = 20;
+        double fare = invoiceGenerator.calculateFare(distance, time);
+        Assertions.assertEquals(70, fare);
+    }
+
+    @Test
+    public void givenLessTimeAndDistance_ShouldReturnMinimumFare() {
+        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+        double distance = 0.1;
+        int time = 1;
+        double fare = invoiceGenerator.calculateFare(distance, time);
+        Assertions.assertEquals(5, fare);
+    }
 }
